132c76ff5d9a2a2359e13c88b53b2ce1a9658533
mttam <95642398+mttam@users.noreply.github.com>
2024-12-03 11:04:31 +0100

    commenti
    Aggiunta commenti

--- py_srt_trans.py
+++ py_srt_trans.py
@@ -25,30 +25,22 @@ TPD: token al giorno
 
 '''
 
-
-# TODO
-# 1 Importare la chiave,il prompt , cosa tradurre e google-gemini V
-# 2 Dividere il file in sotto file V
-# 3 mandare il prompt più la parte da tradurre V
-# 4 unire le parti e scriverle in un nuovo file V
-# 5 creare una classe V
-# 5.1 crea un file requisiti V
-# 6 creare un file unico per chiave,propmt V
-# 7 creare una UI V
-
+# Costanti
 TIME_OUT: int = 60
 MAX_REQUEST: int = 50 
 MAX_ROWS: int = 150
 MODEL_NAME : str = "gemini-1.5-pro"
 
-
+# funzione per la creazione del formato di output
 def formated_final_path(directory,name):
     new_name =  name.split('.')[0] + '_translated.' + name.split('.')[1]
     return f"{directory}/{new_name}"
 
+# funzione per  la formatazione del path iniziale
 def formated_initial_path(directory,name):
     return f"{directory}/{name}"
 
+# funzione per la selezione delle righe da tradurre 
 def select_rows_per_part(rows):
     max_rows_per_part = MAX_ROWS
     total_parts = ceil (rows  / max_rows_per_part)
@@ -86,7 +78,7 @@ class PySrtTrans:
 
         self.update_event = threading.Event()
         
-    
+    # funzioni per il reset delle schermate
     def reset_timer(self):
         self.timer=""
         self.update_event.clear()
@@ -103,6 +95,22 @@ class PySrtTrans:
 
         
     def create_directory(self,directory_name):
+        """
+    Crea la directory specificata se non esiste già.
+    
+    Args:
+        directory_name (str): Nome della directory da creare o verificare.
+    
+    Note:
+        - Verifica se la directory esiste già.
+        - Se esiste, aggiorna il logger con il percorso della directory.
+        - Se non esiste, crea la directory e aggiorna il logger con il nuovo percorso.
+        - Aggiunge l'informazione sulla directory creata o esistente alla lista di directory.
+        - Gestisce eventuali eccezioni durante la creazione della directory.
+    
+    Raises:
+        Exception: Se si verifica un errore durante la creazione o la verifica della directory.
+    """
         try:
             if os.path.exists(directory_name):
                 dir_path = os.path.abspath(os.path.join(self.main_path, directory_name))
@@ -121,35 +129,58 @@ class PySrtTrans:
    
     
 
-    def translate_text(self,text,index,file):
+    def translate_text(self, text, index, file):
+        """
+        Traduce il testo fornito utilizzando l'API di Google Generative AI.
+        
+        Args:
+            text (str): Il testo da tradurre.
+            index (int): L'indice della parte del file che sta essere tradotta.
+            file (str): Il nome del file che sta essere processato.
+        
+        Note:
+            - Configura l'API con la chiave fornita.
+            - Prepara il messaggio da inviare al modello, combinando il testo da tradurre e il prompt.
+            - Imposta le configurazioni per il modello generativo, inclusi temperature, top_p, top_k, e numero massimo di token di output.
+            - Definisce le impostazioni di sicurezza per il modello.
+            - Avvia una sessione di chat con il modello.
+            - Invia il messaggio al modello e ottiene la risposta tradotta.
+            - Registra informazioni sul processo di traduzione nel logger.
+        
+        Returns:
+            str: Il testo tradotto.
+        
+        Raises:
+            Exception: Se si verifica un errore durante il processo di traduzione.
+        """
         genai.configure(api_key=key)
         try:
             message=f"{text}\n{prompt}"
             
             # Create the model
             generation_config = {
-            "temperature": 1,
-            "top_p": 0.95,
-            "top_k": 64,
-            "max_output_tokens": 8192,
-            "response_mime_type": "text/plain",
+                "temperature": 1,
+                "top_p": 0.95,
+                "top_k": 64,
+                "max_output_tokens": 8192,
+                "response_mime_type": "text/plain",
             }
             
             safety_settings : list[str] = [{"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
-                    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
-                    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
-                    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}]
+                        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+                        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+                        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}]
 
             model = genai.GenerativeModel(
-            model_name="gemini-1.5-pro",
-            generation_config=generation_config,
-            safety_settings=safety_settings
-            # See https://ai.google.dev/gemini-api/docs/safety-settings
+                model_name="gemini-1.5-pro",
+                generation_config=generation_config,
+                safety_settings=safety_settings
+                # See https://ai.google.dev/gemini-api/docs/safety-settings
             )
 
             chat_session = model.start_chat(
-            history=[
-            ]
+                history=[
+                ]
             )
             
             self.logger.info(f"Parte srt {index+1} file {file} Inzio Traduzione ")
@@ -165,18 +196,50 @@ class PySrtTrans:
             self.err_message=f"Errore traduzione file  : {str(e)}"
             self.update_event.set()
 
-    def scrivi_file(self,file_path,data):
+    def scrivi_file(self, file_path, data):
+        """
+        Scrive i dati forniti in un file specificato.
+        
+        Args:
+            file_path (str): Percorso completo del file da scrivere.
+            data (list): Lista di stringhe contenenti i dati da scrivere nel file.
+        
+        Note:
+            - Apre il file in modalità scrittura ('w') con codifica UTF-8.
+            - Itera attraverso ogni elemento della lista 'data' e lo scrive nel file.
+            - Registra ogni operazione di scrittura nel logger.
+            - Gestisce eventuali eccezioni durante l'apertura o la scrittura del file.
+        
+        Raises:
+            Exception: Se si verifica un errore durante l'apertura o la scrittura del file.
+        """
         try:
-            with open(file_path,"w",encoding='utf-8') as file:
-                for index,srt in enumerate(data):
+            with open(file_path, "w", encoding='utf-8') as file:
+                for index, srt in enumerate(data):
                     file.write(srt)
                     self.logger.info(f"Scritta parte {index+1} file {file_path}")  
         except Exception as e:
             self.logger.info(f"Errore scrittura file: {str(e)}")
-            self.err_message=f"Errore scrittura file: {str(e)}"
+            self.err_message = f"Errore scrittura file: {str(e)}"
             self.update_event.set()
 
     def select_srt_files(self):
+        """
+        Seleziona uno o più file SRT tramite una finestra di dialogo e li copia nella directory di destinazione.
+        
+        Note:
+            - Apre una finestra di dialogo per permettere all'utente di selezionare uno o più file .srt.
+            - Se nessun file viene selezionato, registra un messaggio di log e imposta un messaggio di errore.
+            - Per ogni file selezionato:
+              - Salva il percorso del file come attributo dell'istanza.
+              - Registra il file selezionato nel logger.
+              - Copia il file nella prima directory specificata in self.directory_names.
+              - Registra il successo della copia nel logger.
+            - Gestisce eventuali eccezioni durante il processo di copia dei file.
+        
+        Raises:
+            Exception: Se si verifica un errore durante la copia dei file.
+        """
         # Apri una finestra di dialogo per selezionare i file .srt
         file_paths = filedialog.askopenfilenames(filetypes=[("SRT Files", "*.srt")])
         
@@ -197,14 +260,32 @@ class PySrtTrans:
     
     
     
-    def split_srt_file(self,input_file):
+    def split_srt_file(self, input_file):
+        """
+        Divide il file SRT di input in parti uguali per la traduzione.
+        
+        Args:
+            input_file (str): Percorso del file SRT da dividere.
+        
+        Note:
+            - Legge il file SRT utilizzando pysrt.
+            - Calcola il numero di righe per parte e il numero totale di parti.
+            - Crea una lista per contenere le parti divise.
+            - Divide il file in parti uguali basate sul numero calcolato.
+            - Converte ogni parte in una stringa e la aggiunge alla lista delle parti.
+            - Registra il numero di token per ogni parte nel logger.
+            - Restituisce la lista delle parti divise.
+        
+        Raises:
+            Exception: Se si verifica un errore durante la divisione del file.
+        """
         try:
             # Leggi il file SRT
             subs = pysrt.open(input_file)
-            self.path_srt=input_file
+            self.path_srt = input_file
             
             # Calcola il numero di righe per parte
-            rows_per_part,num_parts = select_rows_per_part(len(subs))
+            rows_per_part, num_parts = select_rows_per_part(len(subs))
             
             # Inizializza una lista per le parti
             parts = []
@@ -218,45 +299,84 @@ class PySrtTrans:
                 part_str = '\n'.join(str(sub) for sub in part)
                 
                 parts.append(part_str)
-            for index,part in enumerate(parts):
+            
+            # Registra il numero di token per ogni parte
+            for index, part in enumerate(parts):
                 self.logger.info(f"Parte srt {index+1} file {input_file} numero token {self.token_counter(part)}")
+            
             return parts
             
         except Exception as e:
             self.logger.info(f"Errore divisione file : {str(e)}")
-            self.err_message=f"Errore divisione file : {str(e)}"
+            self.err_message = f"Errore divisione file : {str(e)}"
             self.update_event.set()
     
-    def get_file_names(self,directory):
+    
+    def get_file_names(self, directory):
+        """
+        Ottiene i nomi dei file presenti nella directory specificata.
+        
+        Args:
+            directory (str): Percorso della directory da cui ottenere i nomi dei file.
+        
+        Returns:
+            list: Una lista di stringhe contenenti i nomi dei file nella directory.
+        
+        Note:
+            - Utilizza os.listdir() per elencare tutti gli elementi nella directory.
+            - Filtra solo gli elementi che sono effettivamente file usando os.path.isfile().
+            - Restituisce una lista comprensiva dei nomi dei file.
+        
+        Raises:
+            Exception: Se si verifica un errore durante l'accesso alla directory o la lettura del suo contenuto.
+        """
         return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     
     def translates_srt_files(Self):
+        """
+        Traduce i file SRT presenti nella directory di input.
+        
+        Note:
+            - Resetta i messaggi di errore e il timer.
+            - Ottiene la lista dei file SRT da tradurre.
+            - Verifica se il numero di richieste giornaliere non ha superato il limite.
+            - Per ogni file SRT:
+              - Divide il file in parti.
+              - Traduce ogni parte.
+              - Aggiunge la parte tradotta al file finale.
+              - Salva il file tradotto.
+            - Gestisce il caso in cui il limite di richieste giornaliere sia stato raggiunto.
+            - Gestisce eventuali eccezioni durante il processo di traduzione.
+        
+        Raises:
+            Exception: Se si verifica un errore durante il processo di traduzione.
+        """
         try:
             Self.reset_err_message()
             Self.reset_timer()
             file_names_l = Self.get_file_names(Self.directory_path[0]['path'])
-            next_request=0
-            if next_request<=(MAX_REQUEST-Self.g_rpd):
+            next_request = 0
+            if next_request <= (MAX_REQUEST - Self.g_rpd):
                 for file in file_names_l:
                     translated_file = []
-                    parts =  Self.split_srt_file(formated_initial_path(Self.directory_path[0]['path'],file))
-                    next_request= len(parts)
-                    for  index, part in enumerate(parts):
-                        translated_part = Self.translate_text(part,index,file)
-                        Self.g_rpd+=1
+                    parts = Self.split_srt_file(formated_initial_path(Self.directory_path[0]['path'], file))
+                    next_request = len(parts)
+                    for index, part in enumerate(parts):
+                        translated_part = Self.translate_text(part, index, file)
+                        Self.g_rpd += 1
                         translated_file.append(translated_part)
                         sleep(TIME_OUT)
-                    final_file_name = formated_final_path(Self.directory_path[1]['path'],file)
-                    Self.scrivi_file(final_file_name,translated_file)
-                    Self.proced_file=f"File tradotto e salvato {final_file_name}"
+                    final_file_name = formated_final_path(Self.directory_path[1]['path'], file)
+                    Self.scrivi_file(final_file_name, translated_file)
+                    Self.proced_file = f"File tradotto e salvato {final_file_name}"
             else:
                 Self.logger.info(f"Massimo rpd({MAX_REQUEST}) giornaliero raggiunto")
-                Self.err_message=f"Massimo rpd({MAX_REQUEST}) giornaliero raggiunto"
+                Self.err_message = f"Massimo rpd({MAX_REQUEST}) giornaliero raggiunto"
             Self.update_event.set()
-                    
+            
         except Exception as e:
             Self.logger.info(f"Errore processo di traduzione: {str(e)}")
-            Self.err_message=f"Errore processo di traduzione: {str(e)}"
+            Self.err_message = f"Errore processo di traduzione: {str(e)}"
             Self.update_event.set()
 
 
--- py_srt_trans_ui.py
+++ py_srt_trans_ui.py
@@ -32,9 +32,11 @@ class UiPySrtTrans(ctk.CTk):
         self.translate_button = ctk.CTkButton(self, text="Comincia Traduzione", state="disabled", command=threading.Thread(target=self.start_translation).start)
         self.translate_button.pack(pady=20)
         
+        # Create a label to truck progress of translation
         self.progress_label = ctk.CTkLabel(self, text="", wraplength=300)
         self.progress_label.pack(pady=10)
         
+        # Create a label to truck progress of the result
         self.result_label = ctk.CTkLabel(self,text="",wraplength=300)
         self.result_label.pack(pady=10)
         
@@ -43,60 +45,122 @@ class UiPySrtTrans(ctk.CTk):
         self.err_label = ctk.CTkLabel(self, text="", wraplength=300,text_color="red")
         self.err_label.pack(pady=10)
         
+        # Create a threadi to update the UI
         self.update_thread = threading.Thread(target=self.update_labels)
         self.update_thread.start()
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
       
-        
+      
     def update_labels(self):
+        """
+        Aggiorna periodicamente le etichette dell'interfaccia utente in base agli eventi del traduttore.
+        
+        Note:
+            - Esegue un loop continuo finché self.non_stop è True.
+            - Verifica se un evento di aggiornamento è stato impostato dal traduttore.
+            - Se un evento è presente, aggiorna l'interfaccia utente chiamando self.update_gui.
+            - Pulisce l'evento di aggiornamento dopo averlo elaborato.
+            - Introduce una breve pausa di 1 secondo tra gli aggiornamenti.
+        
+        Raises:
+            Exception: Se si verifica un errore durante l'aggiornamento dell'interfaccia.
+        """
         while self.non_stop:
             if self.translator.update_event.is_set():
                 self.after(0, self.update_gui)
                 self.translator.update_event.clear()
             self.after(1000, lambda: None)
 
+    
     def update_gui(self):
+        """
+        Aggiorna l'interfaccia utente con le informazioni più recenti dal traduttore.
+        
+        Note:
+            - Aggiorna l'etichetta del timer con il valore corrente.
+            - Aggiorna l'etichetta di progresso con le informazioni sul file in corso.
+            - Aggiorna l'etichetta di errore con eventuali messaggi di errore.
+        
+        Raises:
+            Exception: Se si verifica un errore durante l'aggiornamento dell'interfaccia.
+        """
         self.timer_label.configure(text=self.translator.timer)
         self.progress_label.configure(text=self.translator.proced_file)
         self.err_label.configure(text=self.translator.err_message)
 
+    
     def on_closing(self):
+        """
+        Gestisce la chiusura dell'applicazione, assicurandosi che tutti i thread in esecuzione vengano terminati correttamente.
         
-        if hasattr(self,'update_thread') and self.update_thread.is_alive():
+        Note:
+            - Verifica se il thread di aggiornamento è attivo e lo fa terminare con un timeout di 5 secondi.
+            - Verifica se il thread di traduzione è attivo e lo fa terminare con un timeout di 5 secondi.
+            - Imposta self.non_stop a False per fermare il loop principale.
+            - Chiude la finestra dell'applicazione.
+        
+        Raises:
+            Exception: Se si verifica un errore durante la chiusura dell'applicazione.
+        """
+        
+        if hasattr(self, 'update_thread') and self.update_thread.is_alive():
             self.update_thread.join(timeout=5)
             
-        
         if hasattr(self.translator, 'translation_thread') and self.translator.translation_thread.is_alive():
             self.translator.translation_thread.join(timeout=5)
-            self.non_stop=False# Give it 5 seconds to stop
+            self.non_stop = False  # Give it 5 seconds to stop
         
         # Close the window
         self.destroy()
     
     
-    def update_gui(self):
-        self.timer_label.configure(text=self.translator.timer)
-        self.progress_label.configure(text=self.translator.proced_file)
-        self.err_label.configure(text=self.translator.err_message)
-        
-    
     def start_translation(self):
+        """
+        Avvia il processo di traduzione dei file SRT selezionati.
+        
+        Note:
+            - Disabilita il pulsante di traduzione per prevenire azioni multiple.
+            - Crea e avvia un thread separato per eseguire la traduzione.
+            - Attende la conclusione del thread di traduzione.
+            - Mostra il risultato della traduzione una volta completata.
+            - Gestisce eventuali eccezioni durante il processo di traduzione.
+        
+        Raises:
+            Exception: Se si verifica un errore durante il processo di traduzione.
+        """
         try:
             self.translate_button.configure(state="disabled")
             self.translator.translation_thread = threading.Thread(target=self.translator.translates_srt_files)
             self.translator.translation_thread.start()
             
-            
+            # Attendi la conclusione del thread di traduzione
             self.translator.translation_thread.join()
             
+            # Mostra il risultato della traduzione
             self.show_result()
         except Exception as e:
+            # Visualizza il messaggio di errore e riabilita il pulsante di traduzione
             self.err_label.configure(text=str(e))
             self.translate_button.configure(state="normal")
             
-    
+  
     def select_files(self):
+        """
+        Seleziona i file SRT da tradurre e aggiorna l'interfaccia utente di conseguenza.
+        
+        Note:
+            - Apre una finestra di dialogo per permettere all'utente di selezionare i file SRT.
+            - Se i file vengono selezionati con successo:
+              - Aggiorna l'etichetta dei file selezionati con il numero di file.
+              - Abilita il pulsante di traduzione.
+            - Se si verifica un errore durante la selezione:
+              - Visualizza il messaggio di errore nell'etichetta di errore.
+              - Abilita il pulsante di traduzione.
+        
+        Raises:
+            Exception: Se si verifica un errore durante il processo di selezione dei file.
+        """
         try:
             self.result_label.configure(text="")
             self.translator.select_srt_files()
@@ -107,7 +171,20 @@ class UiPySrtTrans(ctk.CTk):
             self.err_label.configure(text=str(e))
             self.translate_button.configure(state="normal")
 
+    
     def show_result(self):
+        """
+        Mostra il risultato della traduzione una volta completata.
+        
+        Note:
+            - Crea un'etichetta con il messaggio di completamento e il numero di file tradotti.
+            - Imposta la dimensione del font e lo stile dell'etichetta.
+            - Aggiunge l'etichetta all'interfaccia utente con un padding verticale.
+            - Disabilita il pulsante di traduzione dopo il completamento.
+        
+        Raises:
+            Exception: Se si verifica un errore durante la creazione o l'aggiunta dell'etichetta.
+        """
         result_label = ctk.CTkLabel(self, text=f"Traduzione completata! File tradotti {len(self.translator.get_file_names(self.translator.directory_path[0]['path']))}", font=("Arial", 16))
         result_label.pack(pady=20)
         self.translate_button.configure(state="disabled")  # Disable the button after translation
